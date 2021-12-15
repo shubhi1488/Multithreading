9131b2d640c6c13d923acb80e1679946b7627bf3
HP <shubhi1488@gmail.com>
2021-12-15 23:05:04 +0530

    Multithreading

--- src/StartExp1.java
+++ src/StartExp1.java
@@ -1,2 +1,10 @@
-public class StartExp1 {
+public class StartExp1 extends Thread{
+    public void run(){
+        System.out.println("thread is running");
+    }
+
+    public static void main(String[] args) {
+        StartExp1 obj=new StartExp1();
+        obj.start();
+    }
 }
